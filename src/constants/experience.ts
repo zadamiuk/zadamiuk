3a82375dc34fe52947d2699d175669ea1220595f
Adamiuk <zuzanna.adamiuk@accenture.com>
2023-09-11 13:06:09 +0200

    updated experience data

--- src/constants/experience.ts
+++ src/constants/experience.ts
@@ -7,9 +7,9 @@ export const EXPERIENCE_CONFIG: ExperienceEntity[] = [
     company: 'Accenture',
     date: 'July 2023 - current',
     description: [
-      'Development of application for both Android and iOS',
-      'Research and development of project architecture',
-      'Implementation navigation, pages and reusable components',
+      'development of application for both Android and iOS',
+      'research and development of project architecture',
+      'implementation navigation, pages and reusable components',
     ],
     technologies:
       'React Native, Typescript, React Navigation, React Native Paper, Jest',
@@ -21,9 +21,13 @@ export const EXPERIENCE_CONFIG: ExperienceEntity[] = [
     company: 'Idego Group',
     date: 'October 2021 - June 2023',
     description: [
-      'Development of web platform for monitoring recruitment process',
-      'Development of web platform for monitoring recruitment process',
-      'Implementation communication between frontend and backend',
+      'development of web platform for monitoring recruitment process',
+      'implementation new features using functional components',
+      'taking part in design process focusing on UX',
+      'rewriting application to Typescript',
+      'implementation communication between frontend and backend',
+      'rewriting application to Typescript',
+      'maintenance and fixing occurring bugs',
     ],
     technologies:
       'React.js, Typescript, Redux, Material UI, styled components, Django, Django REST Framework',
@@ -34,9 +38,9 @@ export const EXPERIENCE_CONFIG: ExperienceEntity[] = [
     company: 'Idego Group',
     date: 'January 2023 - June 2023',
     description: [
-      'Development of application for contracts management and work organization',
-      'Creating complex layout based on close collaboration with UX team',
-      'Implementation additional functionalities for HR team such as sending messages to employees',
+      'development of application for contracts management and work organization',
+      'improving code readability by using custom hooks',
+      'cooperating with designers on UX/UI',
     ],
     technologies:
       'React.js, Redux, Material UI, styled components, React Quill, React Toastify',
@@ -47,8 +51,8 @@ export const EXPERIENCE_CONFIG: ExperienceEntity[] = [
     company: 'Idego Group',
     date: 'July 2021 - October 2021',
     description: [
-      'Development of web application which provides access to movies database',
-      'Actualisation of views, refactoring of legacy code',
+      'creating web application providing access to movies database from scratch',
+      'actualisation of views, refactoring of legacy code',
     ],
     technologies: 'Django, React.js, Material UI',
   },
